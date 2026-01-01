4610d3cc3f4fcaeb5c4dce47ae6b03ef4d83ce9b
Akash Rai <arai96756@gmail.com>
2026-01-01 17:46:18 +0530

    I have done

--- Client/Client/src/Pages/Home.jsx
+++ Client/Client/src/Pages/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Herosec from '../Components/Herosec.jsx'
 import Subsec from '../Components/Subsec.jsx'
 import Footer from '../Components/Footer.jsx'
+import Adopt from '../Components/Adopt.jsx'
+import ContactUs from '../Components/ContactUs.jsx'
+
 
 function Home() {
   return (
     <>
       <Herosec />
-        <Subsec />
+      <Subsec />
+      <Adopt />
+      <ContactUs />
       <Footer />
     </>
   )
